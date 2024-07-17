fa66c53bd80f5a6d27a6d24def90a7c1a62d71c5
harshendra <harshsingh.170300@gmail.com>
2024-07-17 17:11:23 +0530

    chat fixes

--- backend/server.js
+++ backend/server.js
@@ -64,36 +64,36 @@ const io = require('socket.io')(server,{
     },
 });
 
-io.on("connection",(socket) => {
+io.on("connection", (socket) => {
     console.log("connected to socket.io");
     socket.on("setup", (userData) => {
         socket.join(userData._id);
-        console.log(userData._id);
         socket.emit("connected");
     });
 
     socket.on("join chat", (room) => {
         socket.join(room);
-        console.log("user joined room" + room);
-    }) 
+    });
 
     socket.on("new message", (newMessageReceived) => {
         var chat = newMessageReceived.chat;
-        if(!chat.users) return console.log("chat.users not defined");
+        if (!chat.users) return console.log("chat.users not defined");
 
         chat.users.forEach(user => {
-            if(user._id === newMessageReceived.sender._id) return;
-
-            socket.in(user._id).emit("message received",newMessageReceived);
+            if (user._id === newMessageReceived.sender._id) return;
+            socket.in(user._id).emit("message received", newMessageReceived);
         });
     });
 
+    socket.on("typing", (room) => {
+        socket.in(room).emit("typing");
+    });
 
-    socket.on("typing",(room) => socket.in(room).emit("typing"));
-    socket.on("stop typing",(room) => socket.in(room).emit("stop typing"));
+    socket.on("stop typing", (room) => {
+        socket.in(room).emit("stop typing");
+    });
 
-    socket.off("setup", () => {
-        console.log("USER DISCONNECTED");
-        socket.leave(userData._id);
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
     });
 });
